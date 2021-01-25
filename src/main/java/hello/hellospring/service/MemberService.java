c9550ca96d9e08ac49157d6437baebaca9ec25bc
Kang Dong Hoon <ypd01018@naver.com>
2021-01-25 23:10:39 +0900

    feat: AOP가 필요한 상황

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -22,10 +22,18 @@ public class MemberService {
     * 회원가입
     * */
     public Long join(Member member) {
-        //같은 이름이 있는 중복 회원x
-        validateDuplicateMember(member); //중복 회원 검증
-        memberRepository.save(member);
-        return member.getId();
+
+        long start = System.currentTimeMillis();
+
+        try {
+            validateDuplicateMember(member); //중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join  = " + timeMs + "ms");
+        }
     }
 
     private void validateDuplicateMember(Member member) {
